53058f4161838e058f5b518e6291c46f3f66337b
João Pedro <moraess.it@gmail.com>
2023-11-19 18:11:01 -0300

    generics

---
+++ generics.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func reverse(slice []int) []int {
+	length := len(slice) - 1
+	newInts := make([]int, len(slice))
+	for i := 0; i < len(slice); i++ {
+		newInts[length] = slice[i]
+		length--
+	}
+	return newInts
+}
+
+func reverse2(slice []string) []string {
+	length := len(slice) - 1
+	newStrings := make([]string, len(slice))
+	for i := 0; i < len(slice); i++ {
+		newStrings[length] = slice[i]
+		length--
+	}
+	return newStrings
+}
+
+func reverseGenerics[T int | string](slice []T) []T {
+	length := len(slice) - 1
+	newSlice := make([]T, len(slice))
+	for i := 0; i < len(slice); i++ {
+		newSlice[length] = slice[i]
+		length--
+	}
+	return newSlice
+}
+
+func main() {
+	slice := []int { 4, 6, 1, 2}
+	slice2 := []string {"A", "N", "D"}
+	/*
+	fmt.Println(reverse(slice))
+	fmt.Println(reverse2(slice2))
+	 */
+	fmt.Println(reverseGenerics(slice))
+	fmt.Println(reverseGenerics(slice2))
+}
+
